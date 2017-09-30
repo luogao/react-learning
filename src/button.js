155c93ab9e106926d07a76381d24ecd00cafd5a6
Roy <gao.luo@honjoinfo.com>
2017-09-30 17:35:21 +0800

    roy
    init

---
+++ src/button.js
@@ -0,0 +1,27 @@
+import React, {
+    Component
+} from 'react';
+
+class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        };
+    }
+
+    handleClick(e) {
+        console.log(this)
+        this.setState({
+            liked: !this.state.liked
+        });
+    }
+    render() {
+        const text = this.state.liked ? 'like' : 'dislike';
+        return ( 
+            <button style={{cursor:'pointer',}} onClick={this.handleClick.bind(this)}> {text} </button>
+        )
+    }
+}
+
+export default Button;
